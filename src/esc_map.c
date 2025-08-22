77a8bc2f6ef2364a1aafa596174c819784d4a255
Paul Trampert <paul.trampert@ptrampert.com>
2025-08-22 19:53:46 -0400

    Add escape map

---
+++ src/esc_map.c
@@ -0,0 +1,25 @@
+#include "esc_map.h"
+
+
+
+// 256-entry lookup table for O(1) escape sequence mapping
+static const unsigned char esc_lookup[256] = {
+    ['a'] = '\a',
+    ['b'] = '\b',
+    ['f'] = '\f',
+    ['n'] = '\n',
+    ['r'] = '\r',
+    ['t'] = '\t',
+    ['v'] = '\v',
+    ['\\'] = '\\',
+    ['\''] = '\'',
+    ['"'] = '"',
+    ['?'] = '\?',
+};
+
+char esc_map_escape(char c) {
+    if (esc_lookup[(unsigned char)c]) {
+        return esc_lookup[(unsigned char)c];
+    }
+    return c;
+}
---
+++ src/esc_map.h
@@ -0,0 +1,8 @@
+#ifndef ESC_MAP_H
+#define ESC_MAP_H
+
+#include <stdint.h>
+
+char esc_map_escape(char c);
+
+#endif // ESC_MAP_H
